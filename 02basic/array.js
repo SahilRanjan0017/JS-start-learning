aab81ab70ccca4de4ec048a6b556c4a5363f09ad
SAHIL RANJAN <98446487+SahilRanjan0017@users.noreply.github.com>
2024-01-09 15:13:11 +0000

    array

---
+++ 02basic/array.js
@@ -0,0 +1,61 @@
+// console.log("Welcome to your second basic lecture")
+
+// array 
+// const myarr=[1,3,4,5,6]
+// const myHero=["shaktiman","naagraj"]
+// const myArr2= new Array(1,2,3,4)
+// myarr.pop()
+// myarr.unshift(9,8)
+// console.log(myarr.include(3));
+
+// console.log(myarr.indexOf(1));
+
+// const newArr= myarr.join()
+
+// const myn1= myArr.slice(1,3)
+// console.log(myn1)
+
+
+        // array second
+
+const marvel_Hero=["thor","Ironman","Spiderman"]
+
+const dc_Hero=["superman","batman","flash"]
+
+// marvel_Hero.push(dc_Hero)
+
+// console.log(marvel_Hero);
+
+// console.log(marvel_Hero[3][2]);
+
+// marvel_Hero.concat(dc_Hero)
+// console.log(marvel_Hero);
+// push or concat same it take it the data and then have to get data by the marvel_Hero[3][1]
+
+//   
+// here you create new array to give all value to treat as element of array
+// console.log(allHero[4])
+ 
+// const all_new_heros=[... marvel_Hero , ... dc_Hero]
+// console.log(all_new_heros);
+
+// real another array 
+// const anothe_array =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_another_array= anothe_array.flat(Infinity)
+
+// console.log(real_another_array)
+
+// console.log(Array.isArray("Sahil"))
+// console.log(Array.from("Sahil")
+// // )
+
+// let score1=100
+// let score2=200
+// let score3=300
+
+// console.log(Array.of(score1,score2,score3))
+
+// so here we saw the Array.is
+//Array.from normal to create the array like "sahil" ['s','a','h','i','l']
+// Array.of here score 1 score 2 score 3 combine in one array 
