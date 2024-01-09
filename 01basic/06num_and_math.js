a45c3cb03835ed9873d9c45f4f463117fddf9e83
SAHIL RANJAN <98446487+SahilRanjan0017@users.noreply.github.com>
2024-01-09 13:09:02 +0000

    maths files

---
+++ 01basic/06num_and_math.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100.432)
+// console.log(balance);
+
+// // 400
+// // [Number: 100]
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+// const hundred= 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-3));
+// console.log(Math.round(3.6))
+// console.log(Math.ceil(9.44))
+// console.log(Math.sqrt(25))
+// console.log(Math.min(3,5,67,8));
+// math library is most use in the math.random 
+// console.log(Math.random())
+// console.log(Math.random()*10+1);
+
+// const min=10
+// const max=20
+
+// console.log(Math.floor(Math.random()*(max-min+1)));
+
+const mini=10
+const maxi=20
+
+console.log(Math.floor(Math.random()*(maxi-mini+1))+mini)
