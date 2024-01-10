62bacde7c9157f612f83fc8fe65d003022c79387
SAHIL RANJAN <98446487+SahilRanjan0017@users.noreply.github.com>
2024-01-10 14:33:08 +0000

    IIFE

---
+++ 03basic/IIFE.js
@@ -0,0 +1,22 @@
+// immediately invoked function expression (IIFE)
+
+// (function chai(){
+//     console.log(`DB Connected`);
+// })() ;
+// global scope pollution problem to solve use IIFE
+// () first function defination 
+// () execution call
+// chai()
+// so we should have to write ; to end then the other is working 
+// (function chaiaurcode(){
+//     console.log("welcome guys") 
+// })();   so here is also ; is important to the end and start new of the function 
+// (()=>{
+//     console.log(`DB connected`)
+// })()     // it is also use as the arrow operator here 
+
+((name)=>{
+    console.log(`Finally the last of the db connection ${name}`);
+})('sahil')
+
+// one more thing in string interpolation after $you should not have to add any space for good working of that .
