43c2aac2752423a77811984defe35124682fcba5
SAHIL RANJAN <98446487+SahilRanjan0017@users.noreply.github.com>
2024-01-11 12:46:54 +0000

    11 jan

--- 02basic/obj.js
+++ 02basic/obj.js
@@ -1,6 +1,12 @@
 // this is the second of the object 
 // const tinderuser = new Object() // this is singleton object
 // it give simple as a empty object
+let tinderuserr={}
+tinderuserr.name="Aayansh"
+tinderuserr.id="233234"
+tinderuserr.issslogingpreviously=false
+// console.log(tinderuserr);
+
 const tinderuser={}
 tinderuser.id="1233"
 tinderuser.name="Aahan"
@@ -18,6 +24,27 @@ tinderuser.islogin=false
 //         }
 //     }
 // }
+// nested object 
+        const hotel={
+            hotelname:"Mouryalok",
+            groundsection:{
+                groudmanger:"rishikesh mahto",
+                leftcorner:{
+                    partytype:"modern family"
+                    ,rightdj:{
+                        djoperator:"sashi mahajan"
+                    }
+                }
+            },
+            floorsection:{
+                floormanger:"ravishankar prasad"
+            },
+            middlehotelsection:{
+                currentownername:"Sahil Ranjan"
+            }
+        }
+    // console.log(hotel.groundsection.leftcorner.rightdj.djoperator)
+        // console.log(hotel.groundsection.groudmanger)
 // So here we are also nested so many of the object in one of the object 
 
 // console.log(regularUser.fullname?.userFullName.firstname);
@@ -26,6 +53,30 @@ tinderuser.islogin=false
 // so here we saw that are work on the full nesting and we can easily nested them in object to object to object and at last get value by . opertator simple.
 
 // object merge and combine 
+        // here we are combining and the marging of the object in the one 
+        const object1={
+            "1":"999",
+            "2":"1999"
+        }
+        const object2={
+            "3":"2999",
+            "4":"3999"
+        }
+        // so Object.assign is the function in which we assign their values 
+        // const finaialarray=object.assign({},object1,object2)
+        // we also use the spread here to the make direct all seprated in one 
+        // const finaialarray=object.assign({},object1,object2)
+        // const finalspreadarray = {...object1,...object2}
+        // const finaialarray=object.assign({},object1,object2)
+        // and if we change the positon also the finalarrayis changning the object1 Reference so problems occurs 
+
+
+        // if the line no 66 run first the object 1 and object assign my object and it give the error on the line 68 
+
+        // console.log(finalspreadarray);
+        // console.log(...object1,...object2);
+        // console.log(finaialarray)
+        
 
 // const obj1={
 //     1:"a",
@@ -53,6 +104,8 @@ tinderuser.islogin=false
 // console.log(objectfinal)
 // now when data base value come so all come in the array 
 
+// as similar as other we can nested all object in the one array 
+
 const userdatavalue=[
     {
         id:1,
@@ -69,24 +122,24 @@ const userdatavalue=[
 ]
 userdatavalue[1].email
 // so we use it by the object of the array 
-console.log(Object.keys(tinderuser))
+// console.log(Object.keys(tinderuser))
 //[ 'id', 'name', 'islogin' ]
 // it give the result as the all key of the selected object
 // so here we saw the object key.which object we pass value in the array so it is so important all work in data bases 
 // we also find values from them 
-console.log(Object.values(tinderuser));
+// console.log(Object.values(tinderuser));
 // it give the value of the object we write in the value 
 // [ '1233', 'Aahan', false ]
 
 // we have also some other function for entries like function 
 
 
-console.log(Object.entries(tinderuser));
+// console.log(Object.entries(tinderuser));
 // so it give the value in array nested
 // [ [ 'id', '1233' ], [ 'name', 'Aahan' ], [ 'islogin', false ] ]
 
 //some time some value not exist so we can do it manual or use property
 
-console.log(tinderuser.hasownproperty('islogged'))
+// console.log(tinderuser.hasownproperty('islogged'))
  // so why this we know is their is key which we want availabe or not 
 
